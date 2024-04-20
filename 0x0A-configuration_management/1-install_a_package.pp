19645e43890837d9796707076530ea48bfa2afc1
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2024-04-20 04:59:23 +0200

    NEW

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# Puppet manifest to install Flask version 2.1.0 using pip3
-
+# Puppet to install Flask version 2.1.0
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
