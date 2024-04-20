0df108a74c9e3008bcd37733b5417e0e7f09d6e7
AhmedNasser <ahmedabdelnasser8888@gmail.com>
2024-04-20 04:56:47 +0200

    style

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,7 @@
 # Puppet manifest to kill a process named 'killmenow' using the exec resource and pkill command
 
-exec { 'pkill':
-  command     => 'pkill killmenow',
-  provider        => 'shell',
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  refreshonly => true,
 }
